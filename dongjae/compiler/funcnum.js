d8c5a5f1a08fabe0bb53d09dbd714eaa0285ab79
jalju mac <dlckswn334@naver.com>
2023-01-22 03:08:28 +0900

    add funcnum.js

---
+++ dongjae/compiler/funcnum.js
@@ -0,0 +1,37 @@
+
+//node funcnum.js RTFN 하면 함수 갯수랑 파라미터 알려줌
+const fs = require('fs');
+const esprima = require('esprima'); // 모듈 추가 가능????????????????
+
+function ReadtheFuncNum(){
+    // Read the JavaScript file
+    const file = fs.readFileSync('./script.js', 'utf-8');
+
+    // Use Esprima to parse the file
+    const ast = esprima.parseScript(file);
+
+    // Iterate over the functions in the file
+    ast.body.forEach(element => {
+        if (element.type === 'FunctionDeclaration'){
+            if(element.id.name !== "ReadtheFuncNum" && element.id.name !== "execFn"){
+                console.log(`Function name: ${element.id.name}`);
+                console.log(`Parameters: ${element.params.map(p => p.name).join(', ')}`);    
+            } 
+           }
+    })
+}
+
+function execFn(fnName, context){
+    args = Array.prototype.slice.call(arguments,2);
+    return context[fnName].apply(context, args);
+}
+
+context = {}
+context["RTFN"]=ReadtheFuncNum;
+
+fnName = process.argv[2];
+inputArgs = process.argv[3];
+args = []
+
+result = execFn(fnName,context,...args);
+
--- dongjae/compiler/script.js
+++ dongjae/compiler/script.js
@@ -1,5 +1,3 @@
-const fs = require('fs'); // 모듈 추가해야되는데 이게 맞는건지 잘 모르겠음
-const esprima = require('esprima'); // 동일
 
 function add(a,b){
     a = Number(a)
@@ -43,24 +41,6 @@ function execFn(fnName, context){
     return context[fnName].apply(context, args);
 }
 
-function ReadtheFuncNum(){
-    // Read the JavaScript file
-    const file = fs.readFileSync('./script.js', 'utf-8');
-
-    // Use Esprima to parse the file
-    const ast = esprima.parseScript(file);
-
-    // Iterate over the functions in the file
-    ast.body.forEach(element => {
-        if (element.type === 'FunctionDeclaration'){
-            if(element.id.name !== "ReadtheFuncNum" && element.id.name !== "execFn"){
-                console.log(`Function name: ${element.id.name}`);
-                console.log(`Parameters: ${element.params.map(p => p.name).join(', ')}`);    
-            } 
-           }
-    });
-}
-
 context={};
 context["add"]=add;
 context["mul"]=mul;
@@ -68,15 +48,12 @@ context["sub"]=sub;
 context["div"]=div;
 context["mod"]=mod;
 context["addthree"]=addthree;
-context["RTFN"]=ReadtheFuncNum;
+
 
 fnName = process.argv[2];
 inputArgs = process.argv[3];
-if(inputArgs !== undefined ){
-    args = inputArgs.split(',')
-}else{
-    args = []
-}
+args = inputArgs.split(',')
+
 
 result = execFn(fnName,context, ...args);
-if(fnName !== "RTFN") console.log(result)
+console.log(result)
